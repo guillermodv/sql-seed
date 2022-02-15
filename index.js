4f7f2e3a917a57eb0552a9dde262aaf6a9f5227e
Guillermo Del Vecchio <guillermo.deve@gmail.com>
2022-02-15 06:55:14 -0300

    feat(db): add conf file

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const apiRouter = require('./router/api');
 
 const app = express();
+
+require('./conf/db');
 
 //Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+// app.use('api', apiRouter);
 
 app.get('/', (req, res) => {
     res.send("RESPONSE HARDCODED");
