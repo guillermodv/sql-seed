72ce29b548e226a17ac506c82929e61ed2bb868d
Guillermo Del Vecchio <guillermo.deve@gmail.com>
2022-02-19 15:09:10 -0300

    feat(babel): adding .env

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import app from "./app";
 import "@babel/polyfill";
+import { config } from "../config";
 
 async function main() {
-  await app.listen(3000);
-  console.log("Server on port 3000");
+  await app.listen(config.PORT);
+  console.log(`Server on port ${config.PORT}`);
 }
 
 main();
