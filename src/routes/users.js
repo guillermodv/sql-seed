626718093a97beb1c6776be5b2275007c2cb3145
Guillermo Del Vecchio <guillermo.deve@gmail.com>
2022-02-20 23:13:06 -0300

    feat(babel): add modify

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 const router = Router();
 
-import { deleteUser, create, getAll } from "../controllers/user";
+import { deleteUser, create, getAll, modifyUser } from "../controllers/user";
 
 router.post("/", create);
 router.get("/", getAll);
 router.delete("/:id", deleteUser);
+router.patch("/:id", modifyUser);
 
 export default router;
